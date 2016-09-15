3a1ab99c7848c2928532b69c6d8f3c67d8875cf7
Lori Burleson <lori.burleson@gmail.com>
2016-09-15 15:14:55 -0400

    converting face cards to numerals = done

--- blackjack_advisor.rb
+++ blackjack_advisor.rb
@@ -1,25 +1,42 @@
-card_number = {}
+card_total = []
 dealer_card = []
 
-2.times {
-puts "Enter your first card, Tex: "
+# convert JQK to numeral 10, A to numeral 11
+def convert_faces_to_num(card_total)
+  card_total.map! { |x| x != "J" ? x : 10}
+  card_total.map! { |x| x != "Q" ? x : 10}
+  card_total.map! { |x| x != "K" ? x : 10}
+  card_total.map! { |x| x != "A" ? x : 11}
+end
+
+# hand has no ace
+def is_hard?
+end
+
+# hand has at least one ace
+def is_soft?
+end
+
+puts "Time to enter your first card, Tex. (Ace = A, Jack, Queen, King = J, Q, K respectively)"
 print "> "
 
-card_one = gets.chomp.to_f
+card_one = gets.chomp.upcase
+
+card_total << card_one
 
 puts "Enter your second card: "
 print "> "
 
-card_two = gets.chomp.to_f
+card_two = gets.chomp.upcase
 
-card_number[card_one] = card_two
-}
+card_total << card_two
 
-print card_number.to_a
+print "#{card_total}"
+
 
 puts "Enter the dealer's first card: "
 print "> "
 
-dealer_card = gets.chomp.to_f
+dealer_card = gets.chomp
 
 # puts "Your optimal move is to #{optimal_move}."
