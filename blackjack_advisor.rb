875729e2255c25cecd4c09833c2f128f4773c087
Lori Burleson <lori.burleson@gmail.com>
2016-09-15 15:51:49 -0400

    checking if a card is a number or not, converting to num OR then running check on if it's a face card or not.

--- blackjack_advisor.rb
+++ blackjack_advisor.rb
@@ -1,12 +1,25 @@
 card_total = []
 dealer_card = []
 
+ #|| card != "K" || card != "Q"
+def convert_to_f(card, card_total)
+  if card.to_f != 0
+    card_total << card.to_f
+  elsif card == "J"
+    card_total << card
+  elsif card == "Q"
+    card_total << card
+  elsif card == "K"
+    card_total << card
+  end
+end
+
 # convert JQK to numeral 10, A to numeral 11
 def convert_faces_to_num(card_total)
-  card_total.map! { |x| x != "J" ? x : 10}
-  card_total.map! { |x| x != "Q" ? x : 10}
-  card_total.map! { |x| x != "K" ? x : 10}
-  card_total.map! { |x| x != "A" ? x : 11}
+  card_total.map! { |x| x != "J" ? x : 10.0}
+  card_total.map! { |x| x != "Q" ? x : 10.0}
+  card_total.map! { |x| x != "K" ? x : 10.0}
+  card_total.map! { |x| x != "A" ? x : 11.0}
 end
 
 # hand has no ace
@@ -22,14 +35,16 @@ print "> "
 
 card_one = gets.chomp.upcase
 
-card_total << card_one
+convert_to_f(card_one, card_total)
+convert_faces_to_num(card_total)
 
 puts "Enter your second card: "
 print "> "
 
 card_two = gets.chomp.upcase
 
-card_total << card_two
+convert_to_f(card_two, card_total)
+convert_faces_to_num(card_total)
 
 print "#{card_total}"
 
