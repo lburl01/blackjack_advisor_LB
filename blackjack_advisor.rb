ecc641cc7a1299f18e4e27a17c1fff91e71b6c30
Lori Burleson <lori.burleson@gmail.com>
2016-09-15 22:30:14 -0400

    refactored getting input in correct format from user.

--- blackjack_advisor.rb
+++ blackjack_advisor.rb
@@ -1,30 +1,53 @@
 all_cards = []
 dealer_card_arr = []
-user_and_dealer_cards = {}
+hand_vs_dealer = {}
 
-def convert_to_f(card, all_cards)
-  if card.to_f != 0
-    all_cards << card.to_f
-  elsif card == "J" || card == "Q" || card == "K" || card == "A"
-    all_cards << card
-  end
-end
+card_value_conversion = {
+  "A" => 11,
+  "J" => 10,
+  "K" => 10,
+  "Q" => 10,
+  "10" => 10,
+  "9" => 9,
+  "8" => 8,
+  "7" => 7,
+  "6" => 6,
+  "5" => 5,
+  "4" => 4,
+  "3" => 3,
+  "2" => 2,
+  "1" => 1
+}
 
-def convert_faces_to_num(all_cards)
-  all_cards.map! { |x| x != "J" ? x : 10.0}
-  all_cards.map! { |x| x != "Q" ? x : 10.0}
-  all_cards.map! { |x| x != "K" ? x : 10.0}
-  all_cards.map! { |x| x != "A" ? x : 11.0}
-end
-
-def run_all_fxns(cards, arrays)
-convert_to_f(cards, arrays)
-convert_faces_to_num(arrays)
-end
-
-def add_two_cards(all_cards, sum)
-  sum = all_cards.first + all_cards.last
-end
+optimal_move_if_hard = {
+  "HIT!" => {
+    5 => [*2..11],
+    6 => [*2..11],
+    7 => [*2..11],
+    13 => [*7..11],
+    14 => [*7..11],
+    15 => [*7..11],
+    16 => [*7..11]
+  },
+  "Double-hit!" => {
+    8 => [*5..6],
+    9 => [*7..11],
+    10 => [*10..11],
+    11 => [*2..11],
+  },
+  "Stand." => {
+    12 => [*4..6],
+    13 => [*2..6],
+    14 => [*2..6],
+    15 => [*2..6],
+    16 => [*2..6],
+    17 => [*2..11],
+    18 => [*2..11],
+    19 => [*2..11],
+    20 => [*2..11],
+    21 => [*2..11]
+  }
+}
 
 # soft hand = has at least one ace
 def is_soft?(all_cards)
@@ -35,46 +58,37 @@ def is_pair?(all_cards)
   all_cards.first == all_cards.last
 end
 
-puts "Time to enter your first card, Tex. (Ace = A, Jack, Queen, King = J, Q, K respectively)"
+puts "Time to enter your first card, Tex. (Ace = A, Jack, Queen, King = J, Q, K respectively.)"
 print "> "
 
 card_one = gets.chomp.upcase
 
-convert_to_f(card_one, all_cards)
-convert_faces_to_num(all_cards)
+card_value_one = card_value_conversion[card_one]
 
-puts "Enter your second card: "
+puts "Enter your second card."
 print "> "
 
 card_two = gets.chomp.upcase
 
-run_all_fxns(card_two, all_cards)
+card_value_two = card_value_conversion[card_two]
 
-sum = all_cards.first + all_cards.last
+full_hand_sum = card_value_one + card_value_two
 
-puts "Sum of user's cards: #{sum}"
-
-puts "Enter the dealer's first card: "
+puts "Now enter the dealer's first card."
 print "> "
 
 dealer_card = gets.chomp.upcase
 
-run_all_fxns(dealer_card, dealer_card_arr)
+card_value_dealer = card_value_conversion[dealer_card]
 
-user_and_dealer_cards[sum] = dealer_card_arr
+hand_vs_dealer[full_hand_sum] = card_value_dealer
 
-puts "#{user_and_dealer_cards}"
+puts "#{hand_vs_dealer}"
 
-if is_pair?(all_cards)
-  puts "pair stuff goes here."
-elsif is_soft?(all_cards) == false
-    if user_and_dealer_cards.key?(5.0) || user_and_dealer_cards.key?(6.0) || user_and_dealer_cards.key?(7.0)
-      puts "HIT!"
-    elsif user_and_dealer_cards.key?(13.0) && user_and_dealer_cards.has_value?([7.0])
-      puts "HIT2!"
-    end
-else is_soft?(all_cards)
-  puts "soft"
-end
-
-# puts "Your optimal move is to #{optimal_move}."
+# if is_pair?(all_cards)
+#   puts "pair stuff goes here."
+# elsif is_soft?(all_cards) == false
+#     puts "hard"
+# else is_soft?(all_cards)
+#   puts "soft"
+# end
